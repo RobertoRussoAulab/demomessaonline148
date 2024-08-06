53428539fcfdd5a80e4725fbf84fb86f91e713b3
Roberto Russo <roberto.russo@aulab.it>
2024-08-06 10:34:34 +0200

    scaffolding finito per la messa online

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>Document</title>
+</head>
+<body>
+    <div class="my-5 robbie-bg-custom">
+        {{$slot}}
+    </div>
+</body>
+</html>
